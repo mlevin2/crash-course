e3aab615f46f3b701c8c4add8d7b7e7d1732f72e
Marshall Levin <marshall_levin@epam.com>
2022-01-24 16:10:59 -0500

    updated navmenu

--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -24,7 +24,7 @@ const NavMenu = () => (
     <li>
       <Link href="/[[...slug]]" as="/registration">
         <a className="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4">
-          Registration
+          Registration!
         </a>
       </Link>
     </li>
